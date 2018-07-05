7983c15a466b0e7b179f9c6a49ea0cffebf7c7b0
lwj1396 <lwj1396@163.com>
2018-07-05 19:47:35 +0800

    re

--- pra/re_test_pattern.py
+++ pra/re_test_pattern.py
@@ -17,4 +17,11 @@ def test_patterns(text, patterns):
 
 
 if __name__ == "__main__":
-    test_patterns('abbaaaabababababa', [('ab', "'a' followed by b")])
+    test_patterns('abbaaaabbababababa',
+            [('ab*', "'a' followed by zero or more b"),
+             ('ab+', "'a' followed by one or more b"),
+             ('ab?', "'a' followed by zero or one b"),
+             ('ab{3}', "'a' followed by three b"),
+             ('ab{2,3}', "'a' followed by tow or three b"),
+             ('ab{2,3}?', "'a' followed by tow or three b")]
+            )
--- re_findall.py
+++
@@ -1,8 +0,0 @@
-import re
-
-text='abbaaabbbbaaaababababaadfda'
-
-pattern = 'ab'
-
-for match in re.findall(pattern, text):
-    print('Found {!r}'.format(match))
